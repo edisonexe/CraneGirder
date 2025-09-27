c0d316cf4c069e4bf7e67335da82da59d9e53477
ray_rid <rid843@mail.ru>
2025-09-27 20:53:22 +0700

    feat: add keyboard input stub

---
+++ Assets/Scripts/InputSystem/KeyboardInput.cs
@@ -0,0 +1,20 @@
+﻿using Crane;
+using UnityEngine;
+
+namespace InputSystem
+{
+    public class KeyboardInput : MonoBehaviour
+    {
+        [SerializeField] private CraneSignals _eventBus;
+
+        private void Update()
+        {
+            _eventBus.Emit(CraneDirection.North, Input.GetKey(KeyCode.W));
+            _eventBus.Emit(CraneDirection.South, Input.GetKey(KeyCode.S));
+            _eventBus.Emit(CraneDirection.East, Input.GetKey(KeyCode.D));
+            _eventBus.Emit(CraneDirection.West, Input.GetKey(KeyCode.A));
+            _eventBus.Emit(CraneDirection.Up, Input.GetKey(KeyCode.Q));
+            _eventBus.Emit(CraneDirection.Down, Input.GetKey(KeyCode.E));
+        }
+    }
+}
